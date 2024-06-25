06ae9fc82f84aa6c21c594a4fb010e14514e200b
testgituhnath <165679034+testgituhnath@users.noreply.github.com>
2024-06-25 04:26:39 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,59 @@
-const form = document.getElementById('form');
-const email = document.getElementById('email');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
-
-form.addEventListener('submit',  e=> { 
-    e.preventDefault();
-    validateInput();
-});
-const setError =(Element,message) =>{
-    const inputControl = Element.parentelement;
-    const errorDisplay = inputControl.queryselector('.error');
-    errorDisplay.innertext = message;
-    inputControl.classlist.add('error');
-    inputControl.classlist.remove('success');
-}
-const setsuccess = Element => {
-    const inputControl = Element.parentelement;
-    const errorDisplay = inputControl.queryselector('.error');
-    errorDisplay = innertext = '';
-    inputControl.classlist.add('success');
-    inputControl.classlist.remove('error');
-};
-const validateInput = ()=>{
-    const usernamevalue =u sername.value.trim();
-    const emailvalue = email.value.trim();
-    const passwordvalue = password.value.trim();
-};
-if (usernamevalue === '') {
-    setError(username, 'required');
-}else{
-    setsuccess(username);
-}
-if (emailvalue === '') {
-    setError(email, 'required');
-}else if(!isvalidemail(emailvalue)) {
-    setError(email, 'provide a valid email address');
-}else{
-    setsuccess(email);
-}
-
-if (password === '') {
-    setError(password, 'password is required');
-}else if(passwordvalue.length <8) {
-    setError(password, 'password word most be atleat 8 character.');
-}else{
-    setsuccess(password);
-}
-if (password2 === '') {
-    setError(password2,'comfirm your password is required');
-}else if(password2value!==passwordvalue) {
-    setError(password, "password2 doesnt not match");
-}else{
-    setsuccess(password2);
-}
+function App() {
+  const [expression, setExpression] = React.useState("");
+  const [answer, setAnswer] = React.useState("0")
+  const display = (Symbol)=>{
+    setExpression(prev => prev + Symbol)
+    if(expression(expression.length-1) === "="){
+      if(/[1-9]/.test(Symbol)){
+        setExpression(Symbol)
+      }else{
+        setExpression(answer + Symbol);
+      }
+    }
+  };
+  const calculate = () => {
+    setAnswer(eval(expression));
+    setExpression((prev)=>prev + "=")
+  };
+  
+  const allClear = ()=>{
+setExpression("");
+setAnswer("");
+  }
+  const clear = () => {
+    setExpression(prev => prev.split("").slice(0, prev.length - 1).join(""));
+    setAnswer("0");
+  };
+  
+    return (
+      <div className="container">
+        <div className="grid">
+          <div className="display">
+<input type="text" value={expression} placeholder="0" disabled></input>
+<div className="Total">{answer}</div>
+          </div>
+          <button onClick={allClear} className="padbutton AC natty">AC</button>
+          <button onClick={clear} className="padbutton C natty">C</button>
+          <button onClick={()=>display("/")} className="padbutton divide">/</button>
+          <button onClick={()=>display("*")} className="padbutton times">*</button>
+          <button onClick={()=>display("7")} className="padbutton seven dark-gray">7</button>
+          <button onClick={()=>display("8")} className="padbutton eight dark-gray">8</button>
+          <button onClick={()=>display("9")} className="padbutton nine dark-gray">9</button>
+          <button onClick={()=>display("-")} className="padbutton minus dark-gray">-</button>
+          <button onClick={()=>display("4")} className="padbutton four dark-gray">4</button>
+          <button onClick={()=>display("5")} className="padbutton five dark-gray">5</button>
+          <button onClick={()=>display("6")} className="padbutton six dark-gray">6</button>
+          <button onClick={()=>display("+")} className="padbutton plus dark-gray">+</button>
+          <button onClick={()=>display("1")} className="padbutton one dark-gray">1</button>
+          <button onClick={()=>display("2")} className="padbutton two dark-gray">2</button>
+          <button onClick={()=>display("3")} className="padbutton three dark-gray">3</button>
+          <button onClick={calculate} className="padbutton equals Agang">=</button>
+          <button onClick={()=>display("0")} className="padbutton zero">0</button>
+          <button onClick={()=>display(".")} className="padbutton dot">.</button>
+        </div>
+      </div>
+    );
+  }
+  
+  ReactDOM.render(<App />, document.getElementById("root"));
+  
